9aa44acfe21f97aedd49feda33689d7ecafb6013
Darrion <darrionkg@gmail.com>
2019-04-26 10:52:12 -0700

    Add CheckForMatches method and tests

--- WordCounter.Tests/ModelTests/ReplaceWordsTests.cs
+++ WordCounter.Tests/ModelTests/ReplaceWordsTests.cs
@@ -11,7 +11,7 @@ namespace WordCounter.Tests
     [TestMethod]
     public void SetPropertyWithConstructor_StoresWords_()
     {
-      ReplaceWords testCase = new ReplaceWords("word", "this is a sentence");
+      ReplaceWords testCase = new ReplaceWords("word", "this is a sentence", "dog");
       //Works. Also returns the correct full sentence string
       //Assert.AreEqual("this is a sentence", testCase.GetSentence());
       Assert.AreEqual(typeof(ReplaceWords), testCase.GetType());
@@ -20,19 +20,38 @@ namespace WordCounter.Tests
     [TestMethod]
     public void MakeSentenceArray_MakesSentenceStringAnArray_StringArray()
     {
-      ReplaceWords testCase = new ReplaceWords("word", "the cat");
+      ReplaceWords testCase = new ReplaceWords("word", "the cat", "dog");
       string[] sentenceArray = testCase.MakeSentenceArray();
       string[] testArray = {"the", "cat"};
       CollectionAssert.AreEqual(sentenceArray, testArray);
     }
 
     [TestMethod]
-    public void CheckForMatches_ChecksForNoMatchAndReturnOriginal_string()
+    public void CheckForMatches_ChecksForNoMatchAndReturnOriginal_int()
     {
-      ReplaceWords testCase = new ReplaceWords("word", "the cat");
+      ReplaceWords testCase = new ReplaceWords("word", "the cat", "dog");
       string[] sentenceArray = testCase.MakeSentenceArray();
       int numOfMatches = testCase.CheckForMatches(sentenceArray);
       Assert.AreEqual(numOfMatches, 0);
+    }
+
+    [TestMethod]
+    public void CheckForMatches_ChecksForOneWordMatch_int()
+    {
+      ReplaceWords testCase = new ReplaceWords("cat", "the cat", "dog");
+      string[] sentenceArray = testCase.MakeSentenceArray();
+      int numOfMatches = testCase.CheckForMatches(sentenceArray);
+      Assert.AreEqual(numOfMatches, 1);
+    }
+
+    [TestMethod]
+    public void CheckForMatches_ChecksForMultipleWordMatches_int()
+    {
+      string longSentence = "the cat Meatball met another cat and became best friends with the cat even though he was an ally cat, it didn't matter because he was a good cat friend";
+      ReplaceWords testCase = new ReplaceWords("cat", longSentence, "dog");
+      string[] sentenceArray = testCase.MakeSentenceArray();
+      int numOfMatches = testCase.CheckForMatches(sentenceArray);
+      Assert.AreEqual(numOfMatches, 5);
     }
   }
 }
--- WordCounter/Models/ReplaceWords.cs
+++ WordCounter/Models/ReplaceWords.cs
@@ -4,11 +4,13 @@ namespace WordCounter
   {
     private string _word;
     private string _sentence;
+    private string _replacementWord;
 
-    public ReplaceWords(string word, string sentence)
+    public ReplaceWords(string word, string sentence, string replacementWord)
     {
       _word = word;
       _sentence = sentence;
+      _replacementWord = replacementWord;
     }
 
     public string GetSentence()
@@ -16,12 +18,6 @@ namespace WordCounter
       return _sentence;
     }
 
-    // public char[] MakeWordArray()
-    // {
-    //   string lowerCaseWord = _word.ToLower();
-    //   char[] wordArray = lowerCaseWord.ToCharArray();
-    //   return wordArray;
-    // }
     public string[] MakeSentenceArray()
     {
       string[] sentenceArray = _sentence.Split(' ');
@@ -31,37 +27,18 @@ namespace WordCounter
     public int CheckForMatches(string[] sentenceArray)
     {
       int matches = 0;
-
+      for(int i = 0; i < sentenceArray.Length; i++) {
+        if(sentenceArray[i].Contains(_word)) {
+          matches++;
+        }
+        else {
+          continue;
+        }
+      }
       return matches;
     }
 
-    // public int CheckForMatches(char[] wordArray, char[] sentenceArray)
-    // {
-    //   int matches = 0;
-    //   // 20 is the max number of characters in a word I'm testing
-    //   char[] tempWord = new char[20];
-    //   for(int i = 0; i < sentenceArray.Length; i++) {
-    //     for(int j = 0; j < wordArray.Length; j++) {
-    //       if(sentence[i] != ' ') {
-    //
-    //         if(sentenceArray[i] == wordArray[j]) {
-    //           tempWord[j] = word[j];
-    //         }
-    //         else {
-    //
-    //         }
-    //       else {
-    //         // if tempWord matches sentenceWord
-    //         Array.Clear(tempWord, 0, 19);
-    //       }
-    //     }
-    //   }
-    //   if(matches != 0) {
-    //     return 1;
-    //   }
-    //   else {
-    //     return 0;
-    //   }
-    // }
+  
+    }
   }
 }
