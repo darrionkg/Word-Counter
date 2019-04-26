954afb7d0ea967b63969943df6c1b7bf0cf4b66b
Darrion <darrionkg@gmail.com>
2019-04-26 09:17:46 -0700

    Make arrays and check for no matches

--- WordCounter.Tests/ModelTests/ReplaceWordsTests.cs
+++ WordCounter.Tests/ModelTests/ReplaceWordsTests.cs
@@ -1,11 +1,39 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ReplaceWords;
+using WordCounter;
+using System;
+using System.Collections.Generic;
 
 namespace WordCounter.Tests
 {
   [TestClass]
   public class ReplaceWordsTest
   {
-    // Test methods go here
+    [TestMethod]
+    public void SetPropertyWithConstructor_StoresWords_()
+    {
+      ReplaceWords testCase = new ReplaceWords("word", "this is a sentence");
+      //Works. Also returns the correct full sentence string
+      //Assert.AreEqual("this is a sentence", testCase.GetSentence());
+      Assert.AreEqual(typeof(ReplaceWords), testCase.GetType());
+    }
+
+    [TestMethod]
+    public void MakeSentenceArray_MakesSentenceStringAnArray_Array()
+    {
+      ReplaceWords testCase = new ReplaceWords("word", "the cat");
+      char[] sentenceArray = testCase.MakeSentenceArray();
+      char[] testArray = {'t','h','e',' ','c','a','t'};
+      CollectionAssert.AreEqual(sentenceArray, testArray);
+    }
+
+    [TestMethod]
+    public void CheckForMatches_ChecksForNoMatchAndReturnOriginal_string()
+    {
+      ReplaceWords testCase = new ReplaceWords("word", "the cat");
+      char[] wordArray = testCase.MakeWordArray();
+      char[] sentenceArray = testCase.MakeSentenceArray();
+      int numOfMatches = testCase.CheckForMatches(wordArray, sentenceArray);
+      Assert.AreEqual(numOfMatches, 0);
+    }
   }
 }
